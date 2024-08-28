723fc894a1c4d24719cc2a9338b964558a465068
Joseph Chen <josephch@mtu.edu>
2024-08-28 02:52:37 -0700

    Create 001_install_apps.sh

---
+++ 001_install_apps.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# File containing the list of applications to install
+APPS_FILE="apps.txt"
+
+# Install applications
+while IFS= read -r line; do
+    # Skip empty lines and comments
+    [[ -z "$line" || "$line" =~ ^# ]] && continue
+    
+    # Determine whether to use Homebrew or Homebrew Cask
+    if [[ "$line" == *"--cask" ]]; then
+        app_name=${line%% --cask}
+        echo "Installing $app_name via Homebrew Cask..."
+        brew install --cask "$app_name"
+    else
+        app_name="$line"
+        echo "Installing $app_name via Homebrew..."
+        brew install "$app_name"
+    fi
+done < "$APPS_FILE"
