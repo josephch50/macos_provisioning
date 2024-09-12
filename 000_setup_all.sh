75b3670b424cd5f1a7941fcbdabe88d11308c88d
Joseph Chen <josephch@mtu.edu>
2024-09-11 22:53:30 -0700

    Update 000_setup_all.sh

--- 000_setup_all.sh
+++ 000_setup_all.sh
@@ -7,6 +7,9 @@ xcode-select –-install
 if ! command -v brew &> /dev/null; then
     echo "Homebrew not found. Installing..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    echo "Homebrew to path"
+    (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/joe/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
 else
     echo "Homebrew is already installed."
 fi
