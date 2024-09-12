50e7c5497c764dbf9959754eac3488a0589f9faa
Joseph Chen <josephch@mtu.edu>
2024-09-11 22:54:09 -0700

    Update 000_setup_all.sh

--- 000_setup_all.sh
+++ 000_setup_all.sh
@@ -8,7 +8,7 @@ if ! command -v brew &> /dev/null; then
     echo "Homebrew not found. Installing..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     echo "Homebrew to path"
-    (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/joe/.zprofile
+    (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> ~/.zprofile
     eval "$(/opt/homebrew/bin/brew shellenv)"
 else
     echo "Homebrew is already installed."
